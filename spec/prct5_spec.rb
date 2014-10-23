3bd6a0415387a0e231eadf4aa667efc42519659e
Carlos <joseka1234@gmail.com>
2014-10-23 17:18:19 +0100

    Modificado codigo de multirespueta.rb

--- spec/prct5_spec.rb
+++ spec/prct5_spec.rb
@@ -4,9 +4,16 @@ require "lib/multirespuesta.rb"
 
 describe MultiRespuesta do
 	before :each do
-		@multi = MultiRespuesta.new
+		@multi = MultiRespuesta.new("Como me llamo",4)
 	end
 	describe "#Introducir pregunta" do
-		it "Introduccion de pregunta correcto"
+		it "Introduccion de pregunta correcto" do
+			@multi.preg.should eq("Como me llamo")
+	end
+	describe "#Introducir numero de respuestas" do
+		it "Numero de respuestas correctas" do
+			@multi.num.should eq(4)
+		end
+	end
 
 end
