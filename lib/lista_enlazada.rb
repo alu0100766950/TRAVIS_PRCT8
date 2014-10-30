780f2433dbef71c2ca4c1802502cb1a3edc384ca
Carlos <joseka1234@gmail.com>
2014-10-30 14:08:00 +0000

    Modificada ListaEnlazada

--- lib/lista_enlazada.rb
+++ lib/lista_enlazada.rb
@@ -1,63 +1,27 @@
-class Struct
-	attr_reader :val, :siguiente
-	attr_writer :val, :siguiente
-	def initializer (val,siguiente)
-		@val = val
-		@siguiente = siguiente
-		@prev = nil
-	end
-end
+Nodo = Struct.new(:val,:siguiente)
 
-class ListaEnlazada
+class ListaEnlazada		#Modifique la lista doblemente enlazada para hacerla simplemente enlazada para estetrabajo
 	attr_reader :head, :tail
 	attr_writer :head, :tail
 	def initializer (vector)
-		@head = Struct.new(vector[0],nil)
-		aux = Struct.new(vector[1],nil)
-		@tail = aux
-		@head.siguiente = @aux
+		@head = Nodo.new(vector[0],nil)
+		aux = Nodo.new(vector[1],nil)
+		@head.siguiente = aux
 		(2..vector.length).each do |i|
-			aux.siguiente = Struct.new(vector[i],nil)
-			@tail = aux.siguiente
+			aux2 = Nodo.new(vector[i],nil)
+			aux.siguiente = aux2
+			aux = aux2
 		end
 	end
-	def pop_fin
-		aux = @tail.val
-		@tail.val = 0
-		@tail = @tail.prev
-		@tail.siguiente = nil
-		return aux
-	end
-	def pop_ini
+	def pop
 		aux = @head.val
 		@head = @head.siguiente
-		@head.prev = nil
 		return aux
 	end
-	def push_fin (val)
-		aux = Struct.new(val,nil)
-		@tail = aux
-		@tail.prev.siguiente = @tail
-		return true
-	end
-	def push_ini (val)
-		aux = Struct.new(val,@head)
+	def push (val)
+		aux = Nodo.new(val,nil)
+		aux.siguiente = @head
+		@head.siguiente = @head
 		@head = aux
-		return true
-	end
-	def push_fin_m (vector)
-		(0..vector.length).each do |i|
-			aux = Struct.new(vector[i],nil)
-			@tail = aux
-			@tail.prev.siguiente = @tail	
-		end		
-		return true
-	end
-	def push_ini_m (vector)
-		(0..vector.length).each do |i|
-			aux = Struct.new(val,@head)
-			@head = aux
-		end
-		return true
 	end
 end
